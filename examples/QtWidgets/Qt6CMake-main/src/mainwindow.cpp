f39c13631283db92a71b3bd6f6a41944b71a1798
Pfanne <pf@nne-mail.de>
2022-08-04 12:33:57 +0200

    Qt demo

---
+++ examples/QtWidgets/Qt6CMake-main/src/mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked() {
+    
+    QString txt = "HelloWorld!";
+
+    /*
+    IedClientError error;
+
+    IedConnection con = IedConnection_create();
+
+    IedConnection_connect(con, &error, "localhost", 102);
+
+    if (error == IED_ERROR_OK) {
+        txt = "connected";
+        IedConnection_close(con);
+    }
+    else {
+        txt = "failed to connect";
+    }
+
+    IedConnection_destroy(con);
+    */
+
+    QMessageBox::about(this, "Title", txt);
+}
