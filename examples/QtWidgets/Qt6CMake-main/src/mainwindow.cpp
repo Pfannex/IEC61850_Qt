449be98b7be0e7e1a2e61cc328babd0978864d50
Pfanne <pf@nne-mail.de>
2022-08-10 16:07:11 +0200

    log working

--- examples/QtWidgets/Qt6CMake-main/src/mainwindow.cpp
+++ examples/QtWidgets/Qt6CMake-main/src/mainwindow.cpp
@@ -15,25 +15,18 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_bu_connect_clicked() {
     
-    QString txt = "HelloWorld!";
-    
-    
     IedClientError error;
-
     IedConnection con = IedConnection_create();
-    
-    IedConnection_connect(con, &error, "localhost", 102);
+    IedConnection_connect(con, &error, "127.0.0.1", 102);
 
     if (error == IED_ERROR_OK) {
-        txt = "connected";
+        ui->tb_serverLog->append("connected to server: 127.0.0.1");
         IedConnection_close(con);
+        ui->tb_serverLog->append("connection closed");
     }
     else {
-        txt = "failed to connect";
+        ui->tb_serverLog->append("failed to connect");
     }
 
     IedConnection_destroy(con);
-    /**/
-
-    QMessageBox::about(this, "Title", txt);
 }
